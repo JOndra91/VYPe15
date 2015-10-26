4e6007c1238bc114886fc69ad39db80fb021c1dc
Ondřej Janošík <j.ondra14@gmail.com>
2015-10-27 00:30:34 +0100

    Simplified instances for SemanticAnalyzer
    Implementation of local for MonadState was missing

--- src/VYPe15/Types/Semantics.hs
+++ src/VYPe15/Types/Semantics.hs
@@ -5,18 +5,17 @@
 module VYPe15.Types.Semantics
 where
 
-import Control.Applicative(Applicative(pure))
-import Control.Monad(Monad(return, (>>=), fail))
-import Control.Monad.Except (ExceptT(ExceptT), runExceptT)
-import Control.Monad.Reader.Class(MonadReader, ask)
-import Control.Monad.State.Class(MonadState, get, put)
-import Control.Monad.Trans.Reader (ReaderT(ReaderT), runReaderT)
-import Control.Monad.Trans.State (State, state, runState)
-import Data.Either (Either(Right,Left))
-import Data.Function (($))
-import Data.Functor(Functor(fmap))
+import Control.Applicative (Applicative(pure, (<*>)))
+import Control.Monad (Monad(fail, return, (>>=)))
+import Control.Monad.Except (ExceptT)
+import Control.Monad.Reader.Class (MonadReader, ask, local)
+import Control.Monad.State.Class (MonadState, get, put)
+import Control.Monad.Trans.Reader (ReaderT)
+import Control.Monad.Trans.State (State)
+import Data.Function (($), (.))
+import Data.Functor (Functor(fmap), (<$>))
 import Data.String (String)
-import Text.Show(Show)
+import Text.Show (Show)
 
 
 import VYPe15.Types.SymbolTable (FunctionTable, VariableTable)
@@ -28,35 +27,27 @@ data SError
 
 -- \r -> \s -> (Either SError a, s)
 
-newtype SemanticAnalyzer a 
+newtype SemanticAnalyzer a
     = SemanticAnalyzer { runSemAnalyzer :: ExceptT SError (ReaderT FunctionTable (State [VariableTable])) a}
 
 -- Could have used generalized newtype deriving but this is a good
--- execise anyway
+-- exercise anyway
 instance Functor SemanticAnalyzer where
-    fmap f b = SemanticAnalyzer $ ExceptT $ ReaderT $ \r -> state $ \s ->
-        case runState (runReaderT (runExceptT $ runSemAnalyzer b) r) s of
-            (Left a, s') -> (Left a, s')
-            (Right a, s') -> (Right (f a), s')
+    fmap f fa = SemanticAnalyzer $ f <$> runSemAnalyzer fa
 
 instance Applicative SemanticAnalyzer where
-    pure a = SemanticAnalyzer $ ExceptT $ ReaderT $ \_ -> state $ \s ->
-        (Right a,s)
+    pure = SemanticAnalyzer . pure
+    a <*> b = SemanticAnalyzer $ runSemAnalyzer a <*> runSemAnalyzer b
 
 instance Monad SemanticAnalyzer where
     return = pure
-    b >>= f = SemanticAnalyzer $ ExceptT $ ReaderT $ \r -> state $ \s ->
-        case runState (runReaderT (runExceptT $ runSemAnalyzer b) r) s of
-            (Left a, s') -> (Left a, s')
-            (Right a, s') -> runState (runReaderT (runExceptT $ runSemAnalyzer $ f a) r) s'
-    fail reason = SemanticAnalyzer $ ExceptT $ ReaderT $ \_ -> state $ \s ->
-        (Left $ SError reason,s)
+    a >>= f = SemanticAnalyzer (runSemAnalyzer a >>= runSemAnalyzer . f)
+    fail = SemanticAnalyzer . fail
 
 instance MonadReader FunctionTable SemanticAnalyzer where
-   ask = SemanticAnalyzer $ ExceptT $ ReaderT $ \r -> state $ \s -> (Right r,s)
+   ask = SemanticAnalyzer ask
+   local f = SemanticAnalyzer . local f . runSemAnalyzer
 
 instance MonadState [VariableTable] SemanticAnalyzer where
-   get = SemanticAnalyzer $ ExceptT $ ReaderT $ \_ -> state $ \s -> (Right s,s)
-   put s = SemanticAnalyzer $ ExceptT $ ReaderT $ \_ -> state $ \_ -> (Right (),s)
-
-
+   get = SemanticAnalyzer get
+   put = SemanticAnalyzer . put
