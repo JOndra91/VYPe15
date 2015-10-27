5886c5845e9536c3390c6c5b9e8c587097213525
Ondřej Janošík <j.ondra14@gmail.com>
2015-10-27 15:53:13 +0100

    Using GeneralizedNewtypeDeriving

--- src/VYPe15/Types/Semantics.hs
+++ src/VYPe15/Types/Semantics.hs
@@ -1,22 +1,21 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module VYPe15.Types.Semantics
 where
 
-import Control.Applicative (Applicative(pure, (<*>)))
-import Control.Monad (Monad(fail, return, (>>=)))
+import Control.Applicative (Applicative)
+import Control.Monad (Monad)
 import Control.Monad.Except (ExceptT)
-import Control.Monad.Reader.Class (MonadReader, ask, local)
-import Control.Monad.State.Class (MonadState, get, put)
+import Control.Monad.Reader.Class (MonadReader)
+import Control.Monad.State.Class (MonadState)
 import Control.Monad.Trans.Reader (ReaderT)
 import Control.Monad.Trans.State (State)
-import Data.Function (($), (.))
-import Data.Functor (Functor(fmap), (<$>))
+import Data.Functor (Functor)
 import Data.String (String)
 import Text.Show (Show)
-
 
 import VYPe15.Types.SymbolTable (FunctionTable, VariableTable)
 
@@ -29,25 +28,4 @@ data SError
 
 newtype SemanticAnalyzer a
     = SemanticAnalyzer { runSemAnalyzer :: ExceptT SError (ReaderT FunctionTable (State [VariableTable])) a}
-
--- Could have used generalized newtype deriving but this is a good
--- exercise anyway
-instance Functor SemanticAnalyzer where
-    fmap f fa = SemanticAnalyzer $ f <$> runSemAnalyzer fa
-
-instance Applicative SemanticAnalyzer where
-    pure = SemanticAnalyzer . pure
-    a <*> b = SemanticAnalyzer $ runSemAnalyzer a <*> runSemAnalyzer b
-
-instance Monad SemanticAnalyzer where
-    return = pure
-    a >>= f = SemanticAnalyzer (runSemAnalyzer a >>= runSemAnalyzer . f)
-    fail = SemanticAnalyzer . fail
-
-instance MonadReader FunctionTable SemanticAnalyzer where
-   ask = SemanticAnalyzer ask
-   local f = SemanticAnalyzer . local f . runSemAnalyzer
-
-instance MonadState [VariableTable] SemanticAnalyzer where
-   get = SemanticAnalyzer get
-   put = SemanticAnalyzer . put
+  deriving (Functor, Applicative, Monad, MonadReader FunctionTable, MonadState [VariableTable])
